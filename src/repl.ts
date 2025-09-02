13c2209e39341deb8de3b742e25abeb913a3cf4a
iahta <153889016+iahta@users.noreply.github.com>
2025-09-02 17:45:59 -0500

    add interface

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,27 @@
+import { createInterface } from 'node:readline';
+
 export function cleanInput(input: string): string[] {
     return input
     .toLowerCase()
     .trim()
     .split(" ")
     .filter(word => word !== "");
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    })
+    rl.prompt();
+    rl.on("line", (line) => {
+        const output = cleanInput(line)
+        if (output.length === 0) {
+            rl.prompt()
+        } else {
+            console.log(`Your command was: ${output[0]}`)
+        }
+        rl.prompt()
+    })
 }
