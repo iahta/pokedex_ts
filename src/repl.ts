beaba6dea1bac848ce814f47094e0e55b870169c
iahta <153889016+iahta@users.noreply.github.com>
2025-09-03 19:28:12 -0500

    exit command added

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,21 @@
 import { createInterface } from 'node:readline';
+import { commandExit } from './command_exit.js';
+
+export type CLICommand = {
+    name: string;
+    description: string;
+    callback: (commands: Record<string, CLICommand>) => void;
+}
+
+export function getCommands(): Record<string, CLICommand> {
+    return {
+        exit: {
+            name: "exit",
+            description: "Exits the pokedex",
+            callback: commandExit,
+        },
+    }
+}
 
 export function cleanInput(input: string): string[] {
     return input
@@ -9,19 +26,31 @@ export function cleanInput(input: string): string[] {
 }
 
 export function startREPL() {
+    const commands = getCommands();
+
     const rl = createInterface({
         input: process.stdin,
         output: process.stdout,
         prompt: "Pokedex > "
-    })
+    });
     rl.prompt();
     rl.on("line", (line) => {
         const output = cleanInput(line)
         if (output.length === 0) {
-            rl.prompt()
-        } else {
-            console.log(`Your command was: ${output[0]}`)
+            rl.prompt();
+            return;
         }
-        rl.prompt()
-    })
+        const commandName = output[0];
+        const command = commands[commandName]
+        if (command) {
+            try {
+                command.callback(commands);
+            } catch (err) {
+                console.error("Error running command:", err);
+            }
+        } else {
+            console.log("Unknown command")
+        }
+        rl.prompt();
+    });
 }
